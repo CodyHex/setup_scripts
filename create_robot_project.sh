07fc26b6e9931a215533834205fc4a86d06f07f6
Peng He <peng.he@r-dex.com>
2019-02-20 14:17:59 -0500

    the create script is tested

--- create_robot_project.sh
+++ create_robot_project.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
-# A bash script to create folders for a whole robotics project.
+# A bash script to create folders for a whole robotics project. 2/20/2019
 
 echo "You should put this script as ~/catkin_ws/src/create_robot_project.sh"
 echo -n "Is the script there? [y/n]:"
 read choice
-if (($choice != y)); then
-	echo "Quit the script."
+if [ $choice != "y" ]
+then
+	echo "Terminated."
 	exit
 fi
 
-echo "Your Robot Name use be used as the prefix for folders."
+echo "Your Robot Name is suggested for the prefix of the project. For a Object Oriented Design Style"
 echo -n "Enter The Robot Name: "
 read robot_name
 
-echo  "Your are going to create a whole project folder structure with->" $robot_name 
+echo  "Your are going to create a project folder structure with prefix->" $robot_name
 echo -n "are you sure? [y/n]:"
 read choice
 
-if (($choice == y))
+if [ $choice = "y" ]
 then
-	mkdir -p $robot_name/{$robot_name"_base",$robot_name"_hri",$robot_name"_viz"}
+	mkdir -p $robot_name/{$robot_name"_base",$robot_name"_bringup",$robot_name"_description",$robot_name"_hri",$robot_name"_legacy"}
+	mkdir -p $robot_name/{$robot_name"_localization",$robot_name"_manager",$robot_name"_message",$robot_name"_navigation",$robot_name"_perception"}
+	mkdir -p $robot_name/{$robot_name"_simulator",$robot_name"_visualization",$robot_name"_debug","external_libraries","install","ots_packages"}
 else
+	echo "Terminated."
 	exit
 fi
 
-echo -n "Do you want README for folder design ideas? [y/n]:"
+echo -n "Download the README for folder structure explanation? [y/n]:"
 read choice
 
 # base_readme="# README\n Create a ROS package with a hardware version name here. E.g. 'beta_robot_driver'.\n"
 # base_readme+="some other things\n"
 
-if (($choice == y)); then
-	touch $robot_name/{$robot_name"_base",$robot_name"_hri",$robot_name"_viz"}/README.md
-	curl -o $robot_name/README.md 'https://github.com/CodyHex/setup_scripts/blob/master/README.md'
-	echo -e $base_readme >> $robot_name/$robot_name"_base"/README.md
+if [ $choice = "y" ]
+then
+	touch $robot_name/{$robot_name"_base",$robot_name"_bringup",$robot_name"_description",$robot_name"_hri",$robot_name"_legacy"}/README.md
+	touch $robot_name/{$robot_name"_localization",$robot_name"_manager",$robot_name"_message",$robot_name"_navigation",$robot_name"_perception"}/README.md
+	touch $robot_name/{$robot_name"_simulator",$robot_name"_visualization",$robot_name"_debug","external_libraries","install","ots_packages"}/README.md
+
+	curl -o $robot_name/README.md 'https://raw.githubusercontent.com/CodyHex/setup_scripts/master/project_structure.md'
+else
+	echo "Terminated."
+	exit
 fi
+
+echo -e "Create Project Folder Structure Finished.\nIt will show you the tree list if you have 'tree' installed."
 
 tree -L 3
 
--- project_structure.md
+++ project_structure.md
@@ -1,8 +1,49 @@
-# README
+# Project Folder Structure Logic 
+
+**Preserver the file name and path to not break the download link**
 
 Create a ROS package with a hardware version name here. E.g. 'beta_robot_driver'.
 
 some other things
 
 
+
+```terminal
+├── create_robot_project.sh
+├── mana
+│   ├── external_libraries
+│   │   └── README.md
+│   ├── install
+│   │   └── README.md
+│   ├── mana_base
+│   │   └── README.md
+│   ├── mana_bringup
+│   │   └── README.md
+│   ├── mana_debug
+│   │   └── README.md
+│   ├── mana_description
+│   │   └── README.md
+│   ├── mana_hri
+│   │   └── README.md
+│   ├── mana_legacy
+│   │   └── README.md
+│   ├── mana_localization
+│   │   └── README.md
+│   ├── mana_manager
+│   │   └── README.md
+│   ├── mana_message
+│   │   └── README.md
+│   ├── mana_navigation
+│   │   └── README.md
+│   ├── mana_perception
+│   │   └── README.md
+│   ├── mana_simulator
+│   │   └── README.md
+│   ├── mana_visualization
+│   │   └── README.md
+│   ├── ots_packages
+│   │   └── README.md
+│   └── README.md
+
+```
 
