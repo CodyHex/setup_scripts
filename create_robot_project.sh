f1b4dfbac4aef92155e16e6ffbed5bd259cceb6a
Peng He <peng.he@r-dex.com>
2019-02-20 15:24:14 -0500

    separate the root readme creation

--- create_robot_project.sh
+++ create_robot_project.sh
@@ -28,7 +28,7 @@ else
 	exit
 fi
 
-echo -n "Download the README for folder structure explanation? [y/n]:"
+echo -n "Create a README for each folder? [y/n]:"
 read choice
 
 # base_readme="# README\n Create a ROS package with a hardware version name here. E.g. 'beta_robot_driver'.\n"
@@ -39,11 +39,19 @@ then
 	touch $robot_name/{$robot_name"_base",$robot_name"_bringup",$robot_name"_description",$robot_name"_hri",$robot_name"_legacy"}/README.md
 	touch $robot_name/{$robot_name"_localization",$robot_name"_manager",$robot_name"_message",$robot_name"_navigation",$robot_name"_perception"}/README.md
 	touch $robot_name/{$robot_name"_simulator",$robot_name"_visualization",$robot_name"_debug","external_libraries","install","ots_packages"}/README.md
-
-	curl -o $robot_name/README.md 'https://raw.githubusercontent.com/CodyHex/setup_scripts/master/project_structure.md'
 else
-	echo "Terminated."
-	exit
+	echo "Skipped Readme Creation."
+fi
+
+echo -n "Download the README for folder structure explanation in the root folder? [y/n]:"
+read choice
+
+if [ $choice = "y" ]
+then
+	curl -o $robot_name/README.md 'https://raw.githubusercontent.com/CodyHex/setup_scripts/master/project_structure.md'
+	echo "Downloaded."
+else
+	echo "Skipped downloading the root readme."
 fi
 
 echo -e "Create Project Folder Structure Finished.\nIt will show you the tree list if you have 'tree' installed."
